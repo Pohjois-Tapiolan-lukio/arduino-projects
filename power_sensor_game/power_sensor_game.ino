9f8cf604663b5142e3669c58135b055c21f1f47a
Jens Pitkänen <jens@neon.moe>
2017-10-14 01:26:39 +0300

    Improve sfx and comments in power_sensor_game

--- power_sensor_game/power_sensor_game.ino
+++ power_sensor_game/power_sensor_game.ino
@@ -39,26 +39,51 @@
 #define PIN_POWER A0
 
 /* How close (%) does the input have to be to trigger yellow light? */
+/* (Default: 30%) */
 #define THRESHOLD_YELLOW 0.3
 /* How close (%) does the input have to be to trigger green light? */
-#define THRESHOLD_GREEN 0.11
+/* (Default: 10%) */
+#define THRESHOLD_GREEN 0.1
+
+/* How long do notes play? */
+/* (Default: 250ms) */
+#define NOTE_LENGTH 100
+
+/* Jingles */
+/* A A - */
+int notesStart[3] = {220, 220, 0};
+/* G H D */
+int notesWin[3]   = {196, 247, 294};
+/* H G#  */
+int notesStop[3]  = {247, 208, 208};
 
 /***************************************
  * Code starts here, modify with care! *
  ***************************************/
 #include "Servo.h"
 
+/* The servo that displays the player's current applied force */
 Servo servo;
 
 /* The game state */
 bool running = false;
-float targetPower;
+float targetPower = -1;
 
 /* Winning conditions */
 bool winning = false;
 unsigned long winTime = 0;
 
+/* Button data */
+bool buttonWasPressed = false;
+
+/* Sound system data */
+int* notes;
+int notesCount;
+int currentIndex = -1;
+unsigned long soundStartTime = 0;
+
 void setup() {
+  /* Setup pins */
   pinMode(PIN_RED, OUTPUT);
   pinMode(PIN_PIEZO, OUTPUT);
   pinMode(PIN_SERVO, OUTPUT);
@@ -68,66 +93,119 @@ void setup() {
 
 void loop() {
   if (buttonPressed()) {
+    /* When the button is pressed, start/stop the game */
     toggleGameState();
   }
   if (running) {
+    /* Update game when running */
     updateGame();
   } else {
+    /* Game is not running, set all leds off and return the servo to normal state */
     digitalWrite(PIN_RED, LOW);
     analogWrite(PIN_YELLOW, 0);
     analogWrite(PIN_GREEN, 0);
     servo.write(90);
   }
+  /* Update the sound system */
+  updateSoundSystem();
 }
 
 void toggleGameState() {
+  /* Switch the game state */
   running = !running;
   if (running) {
-    tone(PIN_PIEZO, 360, 300);
+    /* If the game was started, play starting jingle */
+    playSound(notesStart);
+
+    /* Initialize the game state variables */
+    /* Randomize the target power */
     randomSeed(millis());
     targetPower = random(0, 1024) / 1024.0;
+    /* Ensure the winning state is appropriate */
     winning = false;
     winTime = 0;
   } else {
-    tone(PIN_PIEZO, 110, 300);
+    /* The game was stopped, play stop jingle */
+    playSound(notesStop);
   }
 }
 
+void playSound(int* notes_) {
+  /* Update the notes and set the sound start time to the current time */
+  /* (See updateSoundSystem() for what these variables are used for.) */
+  notes = notes_;
+  soundStartTime = millis();
+  currentIndex = 0;
+}
+
 void updateGame() {
+  /* Poll the sensor */
   float currentPower = analogRead(PIN_POWER) / 1024.0;
+  /* Calculate the distance from the target */
   float distance = abs(targetPower - currentPower);
+  /* If we're within the threshold, set yellow and/or green to a value between 0 and 1 */
+  /* (0 means the player is at the threshold, 1 means the player is right on the target) */
   float yellow = max(0, THRESHOLD_YELLOW - distance) / THRESHOLD_YELLOW;
   float green = max(0, THRESHOLD_GREEN - distance) / THRESHOLD_GREEN;
 
+  /* Display the player's input with the servo */
   servo.write(90 + (int) (90 * (currentPower * 2 - 1)));
 
+  /* If the player is currently winning and has been for a second, win the game and play the jingle */
   if (winning && millis() - winTime > 1000) {
     running = false;
-    tone(PIN_PIEZO, 600, 300);
+    playSound(notesWin);
   }
 
   if (green > 0) {
+    /* Player is within the green threshold, set yellow and red off */
     digitalWrite(PIN_RED, LOW);
     analogWrite(PIN_YELLOW, 0);
+    /* Set the green led on */
     analogWrite(PIN_GREEN, (int) floor(128 + 127 * green));
+    /* If the player wasn't winning already, start counting by setting the time */
     if (!winning) {
       winning = true;
       winTime = millis();
     }
   } else if (yellow > 0) {
+    /* Player is within the yellow threshold, set green and red off */
     digitalWrite(PIN_RED, LOW);
-    analogWrite(PIN_YELLOW, (int) floor(128 + 127 * yellow));
     analogWrite(PIN_GREEN, 0);
+    /* Set the yellow led on */
+    analogWrite(PIN_YELLOW, (int) floor(128 + 127 * yellow));
+    /* Ensure the player is not winning anymore */
     winning = false;
   } else {
+    /* Player is not within any threshold, set only the red led on */
     digitalWrite(PIN_RED, HIGH);
     analogWrite(PIN_YELLOW, 0);
     analogWrite(PIN_GREEN, 0);
+    /* Ensure the player is not winning anymore */
     winning = false;
   }
 }
 
-bool buttonWasPressed = false;
+void updateSoundSystem() {
+  if (currentIndex != -1 && millis() - soundStartTime > 250 * currentIndex) {
+    /* FIXME: Make this 3 non-hardcoded */
+    if (currentIndex >= 3) {
+      /* Add to the currentIndex, if there are no more notes left, set it to -1 */
+      currentIndex = -1;
+      noTone(PIN_PIEZO);
+    } else {
+      /* There are still sounds to play (current index is valid) */
+      /* And NOTE_LENGTH has passed since the last note was played */
+      int note = notes[currentIndex];
+      /* Play the note (if it exists) */
+      if (note != 0) tone(PIN_PIEZO, note);
+      else noTone(PIN_PIEZO);
+      /* Increment currentIndex */
+      currentIndex++;
+    }
+  }
+}
+
 bool buttonPressed() {
   /* Get the button state */
   bool pressed = digitalRead(PIN_BUTTON) == HIGH;
