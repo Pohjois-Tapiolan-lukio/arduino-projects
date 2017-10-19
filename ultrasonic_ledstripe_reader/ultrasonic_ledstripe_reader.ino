365a79a821e99eeb28f82a77e573f8edc072ac6c
Jens Pitkänen <jens@neon.moe>
2017-10-19 17:59:49 +0300

    Add ledstripe_controller, might break something

--- ultrasonic_ledstripe_reader/ultrasonic_ledstripe_reader.ino
+++ ultrasonic_ledstripe_reader/ultrasonic_ledstripe_reader.ino
@@ -1,7 +1,7 @@
 /* Copyright 2017 Jens Pitkanen
  *
- * Permission to use, copy, modify, and/or distribute this software for any 
- * purpose with or without fee is hereby granted, provided that the above 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
@@ -9,7 +9,7 @@
  * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
  * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
  * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
- * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
@@ -40,27 +40,26 @@
 /* (Default: 5m) */
 #define ERROR_MARGIN_ABSOLUTE 500
 
-/* Adafruit_Neopixel led-strip settings.*/
-
-#define  STRIP_PIN 6    //Strip guidance.
-
-#define NUM_LEDS 100     //Choose according to your strip.
-
-#define BRIGHTNESS 100  // Values between 0 ... 255.
-
-#define DEALAY_VAL 10   // Delay for leds.
+/* How often are the leds updated? */
+/* (Default: 24Hz) */
+#define LED_UPDATE_FREQ 24
+/* How much distance variation can be considered "still"? */
+/* (Default: 1cm) */
+#define STILL_DISTANCE_DELTA 1
 
 /***************************************
  * Code starts here, modify with care! *
  ***************************************/
 #include "math.h"
-#include "Adafruit_NeoPixel.h"
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, STRIP_PIN, NEO_GRB + NEO_KHZ800); 
+/* Led management variables */
+unsigned long lastLedUpdateTime = 0;
+double distanceDeltaTotal = 0;
+int distanceDeltaCount = 0;
+double lastDistance = 0;
 
 /* Error checking variable for pollDistance() */
 long lastDuration = -1;
-int lastNumLeds = 0;
 
 void setup() {
   /* Setup the Serial logging and pins */
@@ -70,21 +69,14 @@ void setup() {
   /* Print the Vernier header so the data can be easily copied for later use */
   if (VERNIER_FORMAT) printVernierHeader();
 
-  strip.begin();
-  putStripON();
+  setupLedStrip();
 }
 
 void loop() {
   /* Get the distance from the sensor */
   double distance = getDistance();
+  updateLedDisplay(distance);
 
-  showLedDistance(distance);
-
-  //delay(500);
- // putStripOFF();
-  
-
-  
   /* Print the data in a Vernier-compatible format */
   if (VERNIER_FORMAT) printVernierData(distance, millis());
   else Serial.println(distance);
@@ -126,7 +118,7 @@ long pollDuration(int tryCount) {
   /* NOTE: You could just `return duration` here, and remove the rest of the code. */
   /*       It's all just filtering code for polling errors.                        */
 
-  
+
   /* Check the polled data for errors (if this isn't the first run) */
   if (lastDuration != -1) {
     /* Calculate the delta */
@@ -143,66 +135,46 @@ long pollDuration(int tryCount) {
 
   /* Update lastDuration for use next time */
   lastDuration = duration;
-  
+
   return duration;
 }
 
-/*
-Converts the measured distance range into number of leds ON. Led range from index 5...into NUM_LEDS. First 5 REDleds allways ON.
-Linear model y=kx+B, where x=distance, y=number of leds. Points used (2,200) and (5, NU_LED).
+/* Led display functionality */
 
-*/
-
-int mapLedNumber(double distance) {
-  return int(distance*95/198 + 400/99);
-
+void updateLedDisplay(double distance) {
+  distanceDeltaTotal += distance - lastDistance;
+  lastDistance = distance;
+  distanceDeltaCount++;
+  if (millis() - lastLedupdatetime > 1000.0 / LED_UPDATE_FREQ) {
+    double avgDistanceDelta = distanceDeltaTotal / distanceDeltaCount;
+    showLedDistance(avgDistanceDelta, distance);
+    lastLedUpdatetime = millis();
+    distanceDeltaTotal = 0;
+    distanceDeltaCount = 0;
+  }
 }
 
-
-
-void putStripON() {
-  for(int i=0;i<5;i++){
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    strip.setPixelColor(i, strip.Color(255,0,0)); // First 5 leds are RED.
-
-    strip.show(); // This sends the updated pixel color to the hardware.
-
-    //delay(delayval);
- }
-}
-
-void putStripOFF() {
-  for(int i=5;i<NUM_LEDS;i++){
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    strip.setPixelColor(i, strip.Color(0,0,0)); // First 5 leds are RED.
-
-    strip.show(); // This sends the updated pixel color to the hardware.
-
-    //delay(delayval);
- }
-}
-
-
-void showLedDistance(double distance) {
-
+void showLedDistance(double distanceDelta, double distance) {
+  /* Reset the strip */
+  setLedColor(0, 0, 0);
+  /* Leds */
   int num_leds = mapLedNumber(distance);
 
-  if ( num_leds != lastNumLeds){
-  putStripOFF();
-  //delay(50);
-
-  
-  for(int i=5;i<num_leds;i++){
-
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    strip.setPixelColor(i, strip.Color(0,0,255)); // Distance prested with blue LEDs.
-
-    strip.show(); // This sends the updated pixel color to the hardware.
-
-    //delay(delayval);
+  if (distanceDelta < STILL_DISTANCE_DELTA) {
+    /* The distance is stable, show the distance on the led display */
+    setLedColor(0, 5, 255, 0, 0);
+    setLedColor(5, num_leds, 0, 0, 255);
+  } else {
+    /* The distance is changing a lot, show the loading animation */
+    setLedRainbow(millis() / 1000.0);
   }
-  lastNumLeds = num_leds;
- }
+
+  /* Send the updated pixel color to the hardware after all the changes have been made */
+  updateLeds();
+}
+
+/* Converts the measured distance range into number of leds ON. Led range from index 5...into NUM_LEDS. First 5 RED leds are always ON.
+ * Linear model y=kx+B, where x=distance, y=number of leds. Points used (2,200) and (5, NUM_LED). */
+int mapLedNumber(double distance) {
+  return int(5 + max(0.0, min(1.0, (distance - 2.0) / 198.0)) * (NUM_LED - 5));
 }
