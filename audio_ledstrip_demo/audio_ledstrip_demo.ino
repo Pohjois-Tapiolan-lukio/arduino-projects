99231229e9eeecff982451f8d791f9e3aa50cd0d
Matti Heikkinen <matti.h.heikkinen@eduespoo.fi>
2018-01-12 09:29:02 +0200

    Improve audio led strip demo

--- audio_ledstrip_demo/audio_ledstrip_demo.ino
+++ audio_ledstrip_demo/audio_ledstrip_demo.ino
@@ -19,6 +19,8 @@
 
 #define PIN_AUDIO A0
 #define NUM_LEDS 100
+#define MIN_VOL 0.4
+#define MAX_VOL 0.8
 
 /***************************************
  * Code starts here, modify with care! *
@@ -33,7 +35,7 @@ void setup() {
 
 void loop() {
   /* Get the distance from the sensor */
-  double volume = analogRead(PIN_AUDIO) / 1024.0;
+  double volume = max(0, min(1, (analogRead(PIN_AUDIO) / 1024.0 - MIN_VOL) / (MAX_VOL - MIN_VOL)));
   showLedVolume(volume);
   Serial.println(volume);
 }
@@ -44,8 +46,8 @@ void showLedVolume(double volume) {
   setLedColor(0, 0, 0);
   /* Leds */
   int num_leds = (int) round(volume * (NUM_LEDS - 1));
-  setLedColor(0, 1, 190, 160, 120);
-  setLedColor(1, 1 + num_leds, 230, 130, 80);
+  setLedColor(0, 1, 190, 160, 0);
+  setLedColor(1, 1 + num_leds, 230 * volume * volume, 140 * (1 - volume), 230 * (1 - volume));
 
   /* Send the updated pixel color to the hardware after all the changes have been made */
   updateLeds();
